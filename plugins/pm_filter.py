23455c4552564ee863896dc28ad0f0c4989f2700
teamtkadmin <96689854+teamtkadmin@users.noreply.github.com>
2022-07-14 00:54:45 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -124,7 +124,15 @@ async def filter(client, message):
         if poster:
             await message.reply_photo(photo=poster, caption=f"<b>Here is What I Found In My Database For Your Query {search} ‌‌‌‌‎ ­  ­  ­  ­  ­  </b>", reply_markup=InlineKeyboardMarkup(buttons))
         else:
-            await message.reply_text(f"<b>Here is What I Found In My Database For Your Query {search} ‌‌‌‌‎ ­  ­  ­  ­  ­  </b>", reply_markup=InlineKeyboardMarkup(buttons))
+            await message.reply_text(f"<b>😃 Hi -</b> {message.from_user.mention}
+
+<b>Here is Your Result 👇🏼👇🏼</b>
+
+<b>🗂️ Title :- <a href="https://t.me/+pET5otIzN60yNTc1" >{query}</a> </b>
+<b>💿 Quality :- HD, Blu-Ray, Etc</b>
+<b>📤 Join Us :- <a href="https://t.me/+pET5otIzN60yNTc1" >TK Films 🌟</a> </b>
+
+<b>🔗 Our Channels :- <a href="https://t.me/Tentkotta_Films" >Click Here</a> </b>", reply_markup=InlineKeyboardMarkup(buttons))
 
 @Client.on_message(filters.text & filters.group & filters.incoming & filters.chat(AUTH_GROUPS) if AUTH_GROUPS else filters.text & filters.group & filters.incoming)
 async def group(client, message):
